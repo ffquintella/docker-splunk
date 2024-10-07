adcef66d6da9391f9068887f8ea35feb31ed2eb2
Felipe F Quiontella <felipe@quintella.com>
2024-10-07 08:47:46 -0300

    inc

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-docker build -t ffquintella/docker-splunk:latest --target production .
+
+if test "$#" -ne 2; then
+    echo "please use build.sh <<repo>> <<version>>"
+else
+    docker build -t $1/docker-splunk:$2 --target production .
+fi
+
+
